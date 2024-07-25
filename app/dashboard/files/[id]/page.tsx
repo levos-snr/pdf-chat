92ad2f9b9502dbefe1612bbea44f44ee1725282c
Lewis Odero <lewisodero27@gmail.com>
2024-07-25 18:50:30 +0300

    feat: add files page redirect using id of uploaded file

---
+++ app/dashboard/files/[id]/page.tsx
@@ -0,0 +1,16 @@
+function ChatToFilePage({
+        params: {id},
+}:{
+                params: {
+                        id: string
+                };
+}){
+     return(
+          <div>
+                 <h1>ChatToFilePage</h1>
+                 <h2>{id}</h2>
+          </div>
+     )   
+}
+
+export default ChatToFilePage;
